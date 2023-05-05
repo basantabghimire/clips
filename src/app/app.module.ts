d082764c5fb7d52bd869182c11ed402f01ea59a4
BasantaBabu <basantab.ghimire@gmail.com>
2023-05-05 14:06:46 -0400

    commit ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
-
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-   
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    UserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NavComponent],
+  imports: [BrowserModule, AppRoutingModule, UserModule],
+  providers: [provideEnvironmentNgxMask()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,23 +3,33 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule} from '@angular/forms'
 
-
+import { provideEnvironmentNgxMask, NgxMaskDirective } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component'
 
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+   
   ],
   exports: [
     ModalComponent,
     TabsContainerComponent,
     TabComponent,
+    InputComponent,
+    AlertComponent,
   ],
  
 })
