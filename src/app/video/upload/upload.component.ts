07086edfc6f69ff4ea188f14fc787e02e10238e3
BasantaBabu <basantab.ghimire@gmail.com>
2023-06-10 09:55:41 -0400

    commit shorting clips

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
-import { last } from 'rxjs/operators';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { ClipService } from 'src/app/services/clip.service';
+import {Router} from '@angular/router'
+
+
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css'],
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnDestroy {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
@@ -18,9 +24,11 @@ export class UploadComponent implements OnInit {
   alertMsg = 'Please wait Your clip is being uploaded!!';
   inSubmission = false;
   percentage = 0;
+  showPercentage = false;
+  user: firebase.User | null = null;
+  task?: AngularFireUploadTask;
 
-  title = new FormControl(
-    '',
+  title = new FormControl( '',
     [Validators.required, Validators.minLength(3)]
     // ,
     //  nonNullable: true
@@ -29,13 +37,27 @@ export class UploadComponent implements OnInit {
     title: this.title,
   });
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipsService: ClipService,
+    private router: Router
+  ) {
+    auth.user.subscribe((user) => (this.user = user));
+  }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.task?.cancel()
+  }
 
   storeFile($event: Event) {
+    console.log($event);
+    
     this.isDragover = false;
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    this.file =  ($event as DragEvent).dataTransfer?
+    ($event as DragEvent).dataTransfer?.files.item(0) ?? null:
+    ($event.target as HTMLInputElement).files?.item(0)?? null
+
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
@@ -44,19 +66,63 @@ export class UploadComponent implements OnInit {
     this.nextStep = true;
   }
   uploadFile() {
+    this.uploadForm.disable()
+
     this.showAlert = true;
     this.alertColor = 'blue';
     this.alertMsg = 'Please wait! your clip is being upload.';
     this.inSubmission = true;
+    this.showPercentage = true;
 
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
-    const task = this.storage.upload(clipPath, this.file);
+    this.task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
 
-    task.percentageChanges().subscribe((progress) => {
+    this.task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
     });
-    task.snapshotChanges().pipe(last()).subscribe(console.log);
+
+    this.task
+      .snapshotChanges()
+      .pipe(
+        last(),
+        switchMap(() => clipRef.getDownloadURL())
+      )
+      .subscribe({
+        next: async (url) => {
+          const clip = {
+            uid : this.user?.uid as string,
+            displayName: this.user?.displayName as string,
+            title: this.title.value as string,
+            fileName: `${clipFileName}.mp4`,
+            url,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+          };
+
+          const clipDocRef = await this.clipsService.createClip(clip);
+          console.log(clip);
+
+          this.alertColor = 'green';
+          this.alertMsg = 'Success! Your clips is ready for world';
+          this.showPercentage = false;
+
+          setTimeout(()=>{
+            this.router.navigate([
+              'clip',clipDocRef.id
+            ])
+          },1000)
+        },
+        error: (error) => {
+          this.uploadForm.enable()
+
+          this.alertColor = 'red';
+          this.alertMsg = 'Upload failed please try again later ';
+          this.inSubmission = true;
+          this.showPercentage = false;
+          console.error(error);
+        },
+      });
   }
 }
